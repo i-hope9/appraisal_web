aff1d12b3d40dfec626218a4b80dcf8ff7bf2d28
Eeejeong Kim <ennea9on@gmail.com>
2022-03-07 20:31:37 +0900

    sql: 법원 초기 데이터 스크립트 문 작성

---
+++ src/main/resources/data.sql
@@ -0,0 +1,92 @@
+-- 법원 유형
+INSERT INTO category (name, description, status)
+VALUES ('법원', '감정 법원', 'ENABLE');
+
+INSERT INTO category_item (name, description, category_id, status)
+VALUES ('서울고등법원', '', '1', 'ENABLE');
+INSERT INTO category_item (name, description, category_id, status)
+VALUES ('대전고등법원', '', '1', 'ENABLE');
+INSERT INTO category_item (name, description, category_id, status)
+VALUES ('대구고등법원', '', '1', 'ENABLE');
+INSERT INTO category_item (name, description, category_id, status)
+VALUES ('부산고등법원', '', '1', 'ENABLE');
+INSERT INTO category_item (name, description, category_id, status)
+VALUES ('광주고등법원', '', '1', 'ENABLE');
+INSERT INTO category_item (name, description, category_id, status)
+VALUES ('수원고등법원', '', '1', 'ENABLE');
+INSERT INTO category_item (name, description, category_id, status)
+VALUES ('서울중앙지방법원', '', '1', 'ENABLE');
+INSERT INTO category_item (name, description, category_id, status)
+VALUES ('서울동부지방법원', '', '1', 'ENABLE');
+INSERT INTO category_item (name, description, category_id, status)
+VALUES ('서울남부지방법원', '', '1', 'ENABLE');
+INSERT INTO category_item (name, description, category_id, status)
+VALUES ('서울북부지방법원', '', '1', 'ENABLE');
+INSERT INTO category_item (name, description, category_id, status)
+VALUES ('서울서부지방법원', '', '1', 'ENABLE');
+INSERT INTO category_item (name, description, category_id, status)
+VALUES ('의정부지방법원', '', '1', 'ENABLE');
+INSERT INTO category_item (name, description, category_id, status)
+VALUES ('고양지원', '', '1', 'ENABLE');
+INSERT INTO category_item (name, description, category_id, status)
+VALUES ('인천지방법원', '', '1', 'ENABLE');
+INSERT INTO category_item (name, description, category_id, status)
+VALUES ('인천지방법원 부천지원', '', '1', 'ENABLE');
+INSERT INTO category_item (name, description, category_id, status)
+VALUES ('수원지방법원', '', '1', 'ENABLE');
+INSERT INTO category_item (name, description, category_id, status)
+VALUES ('성남지원', '', '1', 'ENABLE');
+INSERT INTO category_item (name, description, category_id, status)
+VALUES ('여주지원', '', '1', 'ENABLE');
+INSERT INTO category_item (name, description, category_id, status)
+VALUES ('평택지원', '', '1', 'ENABLE');
+INSERT INTO category_item (name, description, category_id, status)
+VALUES ('안산지원', '', '1', 'ENABLE');
+INSERT INTO category_item (name, description, category_id, status)
+VALUES ('안양지원', '', '1', 'ENABLE');
+INSERT INTO category_item (name, description, category_id, status)
+VALUES ('춘천지방법원', '', '1', 'ENABLE');
+INSERT INTO category_item (name, description, category_id, status)
+VALUES ('강릉지원', '', '1', 'ENABLE');
+INSERT INTO category_item (name, description, category_id, status)
+VALUES ('원주지원', '', '1', 'ENABLE');
+INSERT INTO category_item (name, description, category_id, status)
+VALUES ('대전지방법원 논산지원', '', '1', 'ENABLE');
+INSERT INTO category_item (name, description, category_id, status)
+VALUES ('대전지방법원 서산지원', '', '1', 'ENABLE');
+INSERT INTO category_item (name, description, category_id, status)
+VALUES ('대전지방법원 천안지원', '', '1', 'ENABLE');
+INSERT INTO category_item (name, description, category_id, status)
+VALUES ('청주지방법원', '', '1', 'ENABLE');
+INSERT INTO category_item (name, description, category_id, status)
+VALUES ('충주지원', '', '1', 'ENABLE');
+INSERT INTO category_item (name, description, category_id, status)
+VALUES ('제천지원', '', '1', 'ENABLE');
+INSERT INTO category_item (name, description, category_id, status)
+VALUES ('대구지방법원', '', '1', 'ENABLE');
+INSERT INTO category_item (name, description, category_id, status)
+VALUES ('대구지방법원 서부지원', '', '1', 'ENABLE');
+INSERT INTO category_item (name, description, category_id, status)
+VALUES ('부산지방법원', '', '1', 'ENABLE');
+INSERT INTO category_item (name, description, category_id, status)
+VALUES ('부산지방법원 동부지원', '', '1', 'ENABLE');
+INSERT INTO category_item (name, description, category_id, status)
+VALUES ('부산지방법원 서부지원', '', '1', 'ENABLE');
+INSERT INTO category_item (name, description, category_id, status)
+VALUES ('울산지방법원', '', '1', 'ENABLE');
+INSERT INTO category_item (name, description, category_id, status)
+VALUES ('창원지방법원', '', '1', 'ENABLE');
+INSERT INTO category_item (name, description, category_id, status)
+VALUES ('진주지원', '', '1', 'ENABLE');
+INSERT INTO category_item (name, description, category_id, status)
+VALUES ('광주지방법원', '', '1', 'ENABLE');
+INSERT INTO category_item (name, description, category_id, status)
+VALUES ('광주지방법원 목포지원', '', '1', 'ENABLE');
+INSERT INTO category_item (name, description, category_id, status)
+VALUES ('광주지방법원 순천지원', '', '1', 'ENABLE');
+INSERT INTO category_item (name, description, category_id, status)
+VALUES ('전주지방법원', '', '1', 'ENABLE');
+INSERT INTO category_item (name, description, category_id, status)
+VALUES ('군산지원', '', '1', 'ENABLE');
+INSERT INTO category_item (name, description, category_id, status)
+VALUES ('제주지방법원', '', '1', 'ENABLE');
